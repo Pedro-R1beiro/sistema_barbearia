2d3570e8a2fa99f4528708a2786d368c5de63e87
Kaique790 <kiaqbs@gmail.com>
2025-05-27 19:16:48 -0300

    feat: react query and axios configured

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 import { ThemeProvider } from "./components/themes/ThemeProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+import { Toaster } from "sonner";
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <Toaster richColors />
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 }
