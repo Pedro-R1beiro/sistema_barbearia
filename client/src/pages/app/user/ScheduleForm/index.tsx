13fe12107ee5946c5c4949e753f87390ab181dd2
Kaique790 <kiaqbs@gmail.com>
2025-06-12 21:12:06 -0300

    feat(ScheduleForm): validations in user schedule form

--- client/src/pages/app/user/ScheduleForm/index.tsx
+++ client/src/pages/app/user/ScheduleForm/index.tsx
@@ -9,27 +9,32 @@ import { TimeSelect } from "./components/TimeSelect";
 import { useMutation } from "@tanstack/react-query";
 import { Card, CardContent } from "@/components/ui/card";
 import { registerAppointment } from "@/api/register-appointment";
-import { BarberSelect } from "./components/BarberSelect";
 import { toast } from "sonner";
 import { queryClient } from "@/lib/react-query";
+import { BarberSelect } from "./components/BarberSelect";
 
 const scheduleFormDataSchema = z.object({
-  barber: z.string(),
-  services: z.array(z.number()),
+  barber: z.string().min(1, "Selecione um barbeiro"),
+  services: z.array(z.number()).min(1, "Selecione algum serviço"),
   date: z.date(),
-  startTime: z.string(),
+  startTime: z.string().min(5, "selecione um horário"),
 });
 
 export type ScheduleFormData = z.infer<typeof scheduleFormDataSchema>;
 
 export function ScheduleForm() {
-  const { handleSubmit, control, watch, reset } = useForm<ScheduleFormData>({
+  const {
+    handleSubmit,
+    control,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<ScheduleFormData>({
     resolver: zodResolver(scheduleFormDataSchema),
     defaultValues: {
       barber: "",
       services: [],
       startTime: "",
-      date: new Date(),
     },
   });
 
@@ -70,17 +75,27 @@ export function ScheduleForm() {
       </h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="min-w-full space-y-7 pb-8 lg:flex lg:max-w-2xl lg:flex-1 lg:items-center lg:justify-center lg:gap-15"
+        className="min-w-full space-y-7 pb-8 lg:flex lg:max-w-2xl lg:flex-1 lg:items-center lg:items-start lg:justify-center lg:gap-15"
       >
         <div className="space-y-7 lg:flex-1">
           <div>
             <h3 className="text-lg font-bold">Selecione uma data</h3>
             <DatePicker control={control} />
+            {errors.date && (
+              <p className="text-sm text-red-500 dark:text-red-400">
+                Selecione uma data
+              </p>
+            )}
           </div>
 
           <div>
             <h3 className="text-lg font-bold">Selecione serviços</h3>
             <SelectServices control={control} />
+            {errors.services && (
+              <p className="text-sm text-red-500 dark:text-red-400">
+                {errors.services.message}
+              </p>
+            )}
           </div>
         </div>
 
@@ -89,6 +104,11 @@ export function ScheduleForm() {
             <div>
               <h3 className="text-lg font-bold">Selecione um barbeiro</h3>
               <BarberSelect control={control} watch={watch} />
+              {errors.barber && (
+                <p className="text-sm text-red-500 dark:text-red-400">
+                  {errors.barber.message}
+                </p>
+              )}
             </div>
           ) : (
             <Card className="p-2.5">
@@ -105,6 +125,11 @@ export function ScheduleForm() {
             <div>
               <h3 className="text-lg font-bold">Selecione um horário</h3>
               <TimeSelect control={control} watch={watch} />
+              {errors.startTime && (
+                <p className="text-sm text-red-500 dark:text-red-400">
+                  {errors.startTime.message}
+                </p>
+              )}
             </div>
           ) : (
             <Card className="p-2.5">
