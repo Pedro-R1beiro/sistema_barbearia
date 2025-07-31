dcceafe4f251b58a875062f41daa7d9823965b8d
Kaique790 <kiaqbs@gmail.com>
2025-07-31 17:42:50 -0300

    feat(Sale/Home.tsx): add new interface

---
+++ client/src/components/MiniCard.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import type { IconLookup } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "motion/react";
+
+interface MiniCardProps {
+  firstTitle: string;
+  lastTitle: string;
+  classname?: string;
+  delay?: number;
+  transition: number;
+  icon: IconLookup;
+}
+
+export function MiniCard({
+  firstTitle,
+  lastTitle,
+  icon,
+  classname = "",
+  delay = 1,
+  transition = 1,
+}: MiniCardProps) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        ease: "easeOut",
+        delay: delay,
+        opacity: {
+          duration: transition * 0.9,
+        },
+      }}
+      className={cn(
+        "bg-foreground text-background flex w-[105px] flex-col gap-1 rounded-md p-2 px-3.5 pt-3.5 duration-300 hover:scale-106",
+        classname,
+      )}
+    >
+      <FontAwesomeIcon icon={icon} size="xl" />
+      <div className="flex flex-col text-center">
+        <div className="font-bold">{firstTitle}</div>
+        <div>{lastTitle}</div>
+      </div>
+    </motion.div>
+  );
+}
--- client/src/pages/Sale/components/Home.tsx
+++ client/src/pages/Sale/components/Home.tsx
@@ -1,13 +1,19 @@
 import { ContactButton } from "@/components/ContactButton";
 import { Button } from "@/components/ui/button";
 
-import moustacheIcon from "@/assets/moustache-icon.svg";
-
-import { SeparationArrow } from "@/components/SeparationArrow";
 import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 import bg from "@/assets/sale-bg.svg";
 import { useNavigate } from "react-router";
+import { MiniCard } from "@/components/MiniCard";
+import {
+  faCalendar,
+  faClock,
+  faScissors,
+} from "@fortawesome/free-solid-svg-icons";
+import { motion } from "motion/react";
+
+const MotionButton = motion(Button);
 
 export function Home() {
   const navigate = useNavigate();
@@ -16,43 +22,115 @@ export function Home() {
     <section
       style={{ backgroundImage: `url(${bg})` }}
       id="home"
-      className="bg-foreground/40 dark:bg-background/0 flex h-screen flex-col items-center bg-cover bg-center bg-no-repeat px-4 pt-40 md:mt-0 md:gap-12 md:pb-0"
+      className="bg-foreground/40 dark:bg-background/0 flex h-screen flex-col items-center bg-cover bg-center bg-no-repeat px-4 pt-35 md:mt-0 md:gap-12 md:pb-0"
     >
-      <div className="mx-auto space-y-4 text-center">
-        <h1 className="mx-auto mb-6 flex w-fit gap-3 text-center text-4xl font-bold tracking-tight">
-          BARBER SHOP{" "}
-          <img
-            src={moustacheIcon}
-            alt="Ícone de bigode pontudo"
-            className="dark:bg-background bg-foreground w-10 rounded-full p-2"
-          />
+      <div className="text-center">
+        <span className="sr-only">@Sua barbearia</span>
+        <h1 className="mb-1 text-2xl font-bold">
+          {"@Sua barbearia".split("").map((letter, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                delay: 0.09 * index,
+                duration: 0.5,
+              }}
+            >
+              {letter}
+            </motion.span>
+          ))}
         </h1>
-        <p className="mb-6 text-[1.75rem] font-light">
-          Cabelo, barba e tudo que te deixa <br className="hidden md:inline" />{" "}
-          em dia.
-        </p>
 
-        <Button
+        <p className="max-w-[315px] text-lg font-light">
+          {"Agende seu horário e cuide do que faz você quem você é."
+            .split("")
+            .map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  delay: 0.02 * index,
+                  duration: 0.4,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          <span className="sr-only">
+            Agende seu horário e cuide do que faz você quem você é.
+          </span>
+        </p>
+      </div>
+
+      <div className="mt-14 flex w-[240px] flex-col gap-3">
+        <MotionButton
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            ease: "easeOut",
+            opacity: {
+              duration: 2 * 0.8,
+            },
+          }}
           onClick={() => navigate("/sign-up")}
-          className="bg-foreground text-background text-md mt-2 w-full p-6 font-bold md:max-w-114"
+          className="border-background bg-foreground font-semibold"
         >
           Agende seu horário
-        </Button>
-        <Button
+        </MotionButton>
+        <MotionButton
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.4,
+            ease: "easeOut",
+            delay: 0.2,
+          }}
           onClick={() => navigate("/sign-in")}
           variant="secondary"
-          className="text-md mt-2 w-full p-6 font-bold md:max-w-114 dark:bg-[#303030] dark:hover:bg-[#404040]"
+          className="border-background border-2 py-4.5 font-semibold"
         >
-          Já tem horários marcados?
-        </Button>
-
-        <div className="mx-auto mt-6 flex w-fit items-center justify-center gap-3">
-          <ContactButton link="instagram.com" icon={faInstagram} />
-          <ContactButton link="whatsapp.api.client" icon={faWhatsapp} />
-        </div>
+          Gerenciar meus horários
+        </MotionButton>
       </div>
-      <div className="mt-auto w-full max-w-6xl">
-        <SeparationArrow />
+      <motion.div
+        initial={{ opacity: 0, y: 70 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.6,
+          ease: "easeOut",
+          delay: 0.4,
+        }}
+        className="mt-4.5 flex gap-3"
+      >
+        <ContactButton link="" icon={faInstagram} />
+        <ContactButton link="" icon={faWhatsapp} />
+      </motion.div>
+
+      <div className="mt-auto mb-6 flex w-full max-w-[372px] items-center justify-between">
+        <MiniCard
+          firstTitle="A melhor"
+          lastTitle="barberia"
+          icon={faScissors}
+          delay={0.2}
+          transition={1}
+        />
+        <MiniCard
+          firstTitle="Rápido"
+          lastTitle="e fácil"
+          icon={faClock}
+          delay={0.3}
+          transition={2}
+        />
+        <MiniCard
+          firstTitle="Gerencie"
+          lastTitle="horários"
+          icon={faCalendar}
+          delay={0.4}
+          transition={3}
+          classname="bg-amber-400"
+        />
       </div>
     </section>
   );
